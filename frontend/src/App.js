0c574534c42410d88e0a1719ffa7a9f79e17f25a
Bruno Alves <bruno6821@gmail.com>
2022-12-01 05:58:11 -0300

    Implementaando o Reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import "./index.css";
 
 // ROUTES
-import { BrowserRouter, Navigate, Router, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
