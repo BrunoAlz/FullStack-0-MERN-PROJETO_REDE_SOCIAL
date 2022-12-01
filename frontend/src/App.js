8f2b2c21d9fc3d7a9b5722808f018e6feabe7c1d
Bruno Alves <bruno6821@gmail.com>
2022-12-01 05:16:41 -0300

    Criando os Diretórios de Paginas e Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import './index.css';
+import "./index.css";
+
+// ROUTES
+import { BrowserRouter, Navigate, Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
