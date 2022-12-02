b1028487522d38d4b00218ab7ca46879cce928a9
Bruno Alves <bruno6821@gmail.com>
2022-12-01 21:13:09 -0300

    Implementação Completa do Registro e Login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,39 @@
-
+// MUI
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 500px)");
 
-export default Login
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          MikeBook
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" textAlign="center" sx={{mb: "1.5rem"}}>
+          Bem vindo ao MikeBook! 
+        </Typography>
+        <Form>
+
+        </Form>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
